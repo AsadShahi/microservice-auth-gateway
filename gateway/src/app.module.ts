d3c32973aa48b733f85ba3a86c8c2003e754568f
unknown <asadshahi804@gmail.com>
2025-04-07 11:14:57 +0430

    config and register new client for microservices

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { join } from 'path';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+         name: 'AUTH_PACKAGE',
+         transport:Transport.GRPC,
+         options:{
+          package:'auth',
+          protoPath:join(__dirname, '../../proto/auth.proto'),
+          url: 'localhost:50051',
+         }
+      }
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
