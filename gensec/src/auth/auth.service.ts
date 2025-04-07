f509806f8eb1eb2abb2ee75475c9fa587bac6c5e
unknown <asadshahi804@gmail.com>
2025-04-07 12:58:55 +0430

    register method in authservice handled

--- gensec/src/auth/auth.service.ts
+++ gensec/src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  Register(data: { username: string; password: string }): { message: string } {
+    if (data.username === 'error') {
+      throw new RpcException('Username is not allowed');
+    }
+    return { message: 'User registered successfully' };
+  }
+}
